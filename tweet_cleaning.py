e4faf0861ea39a1bf8e6a49f1295935a16a28873
Túlio <88946365+tuliosg@users.noreply.github.com>
2022-05-03 09:31:22 -0300

    Alteração em loop

--- tweet_cleaning.py
+++ tweet_cleaning.py
@@ -3,21 +3,26 @@ import pandas as pd
 import cleaning
 
 ##LIMPANDO DADOS
-#Abre o arquivo e gera um novo .csv com os dados limpos
-with open('Dados-Fanfic_(romance)(CSV).csv', encoding='utf-8') as arquivo:
+#Abre o arquivo contendo os dados coletados e gera um novo .csv após a limpeza
+with open('Dados-Twitter.csv', encoding='utf-8') as arquivo:
     dados = csv.reader(arquivo, delimiter=';')
 
+    #O novo arquivo foi organizado contendo uma classificação do tipo de tweet e o texto do tweet.
+    #Isso pode ser alterado a depender do objetivo
     colunas = ["Classificacao", "Tweet"]
     linhas = []
-
+    
+    #Lê e limpa linha por linha do arquivo
     for i in dados:
+        #i[3] -> posição do arquivo "Dados-Twitter.csv" (4ª coluna) onde está o texto do tweet a ser limpo
         tweet = i[3]
         tweet = cleaning.limpa_str(tweet)
-        linhas.append(["Romance", tweet])
+        linhas.append(["classificação", tweet])
         #print(tweet)
 
     data_frame = pd.DataFrame(linhas, columns=colunas)
 
-    data_frame.to_csv("Dados-Fanfic(romance).csv", encoding="UTF-8")
+    #Gerando .csv com os arquivos limpos
+    data_frame.to_csv("Dados-Twitter(limpos).csv", encoding="UTF-8")
 
 arquivo.close()
